2cab43d8db44e57a7be6f7275dbd9b342aa0b50d
少林功夫好 <982282139@qq.com>
2020-10-26 17:27:02 +0800

    10/26

--- miniprogram/components/blog-ctrl/blog-ctrl.js
+++ miniprogram/components/blog-ctrl/blog-ctrl.js
@@ -1,11 +1,12 @@
 // components/blog-ctrl/blog-ctrl.js
 let userInfo = {}
+const db =wx.cloud.database()
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    blogId:String
   },
   externalClasses: ['iconfont', 'icon-pinglun', 'icon-fenxiang'],
   /**
@@ -14,7 +15,7 @@ Component({
   data: {
     // 登录组件是否显示
     loginShow: false,
-    modalShow:false
+    modalShow: false
   },
 
   /**
@@ -23,16 +24,15 @@ Component({
   methods: {
     onComment: function () {
       // 判断用户是否授权
-      console.log('点击了')
       wx.getSetting({
         success: (res) => {
-          if (res.authSetting['scope.userInfo']) {  //用户未授权
+          if (res.authSetting['scope.userInfo']) { //用户未授权
             wx.getUserInfo({
               success: (res) => {
                 userInfo = res.userInfo
                 // 显示评论弹出层
                 this.setData({
-                  modalShow:true
+                  modalShow: true
                 })
               }
             })
@@ -45,20 +45,60 @@ Component({
       })
     },
     // 授权成功  显示评论框  隐藏授权框
-    onloginSuccess(){
+    onloginSuccess(event) {
+      userInfo=event.detail
       this.setData({
-        loginShow:false,
-      },()=>{
+        loginShow: false,
+      }, () => {
         this.setData({
-          modalShow:true
+          modalShow: true
         })
       })
     },
     // 授权失败
-    onloginfail(){
+    onloginfail() {
       wx.showModal({
-        title:'授权用户才能进行评价',
-        content:''
+        title: '授权用户才能进行评价',
+        content: ''
+      })
+    },
+    onInput(event) {
+      //用户输入内容
+      this.setData({
+        content: event.detail.value
+      })
+    },
+    // 用户点击发送评论
+    onSend() {
+      let content = this.data.content
+      if (content.trim() === '') {
+        wx.showToast({
+          title: '输入内容不能为空',
+          content: ''
+        })
+        return
+      }
+      wx.showLoading({
+        title: '评价中',
+        mask:true
+      })
+      db.collection('blog-comment').add({
+        data:{
+          content,
+          createTime:db.serverDate(),
+          blogId:this.properties.blogId,
+          nikeName:userInfo.nickName,
+          avatarUrl:userInfo.avatarUrl
+        }
+      }).then((res)=>{
+        wx.hideLoading()
+        wx.showToast({
+          title: '评论成功',
+        })
+        this.setData({
+          modalShow:false,
+          content:''
+        })
       })
     }
   }
