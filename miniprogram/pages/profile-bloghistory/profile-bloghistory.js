536e5c6804b410d2c6dbbd653f8724172586b2e8
少林功夫好 <982282139@qq.com>
2020-11-02 17:23:03 +0800

    11/2

--- miniprogram/pages/profile-bloghistory/profile-bloghistory.js
+++ miniprogram/pages/profile-bloghistory/profile-bloghistory.js
@@ -1,5 +1,8 @@
 // pages/profile-bloghistory/profile-bloghistory.js
 const MAX_LILMIT=10
+const blogCollection=wx.cloud.database().collection('blog')
+import formatTime from '../../utils/formatTime.js'
+const app=getApp()
 Page({
 
     /**
@@ -13,7 +16,8 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        this.getBlogListHistory()
+        this.getBlogListHistory() //云函数调用
+        // this.getBlogListByminPro() //小程序调用
     },
 
     /**
@@ -61,9 +65,16 @@ Page({
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
-
+    onShareAppMessage: function (event) {
+        const blog=event.target.dataset.blog
+        return{
+            title:blog.content, 
+            path:`/pages/blog-comment/blog-comment?blogId=${blog._id}`,
+            imageUrl:blog.img[0]
+        }
     },
+    // 调用云函数
+    //方法1
     getBlogListHistory:function(){
         wx.showLoading({
           title: '加载中',
@@ -76,11 +87,44 @@ Page({
                 count:MAX_LILMIT
             }
         }).then((res)=>{
-            console.log(res)
+            let bloglist=res.result
+            for(let i=0,len=bloglist.length;i<len;i++){
+                // bloglist[i].createTime=bloglist[i].toString()
+                bloglist[i].createTime = formatTime(new Date(bloglist[i].createTime))
+            }
             this.setData({
-                blogList:this.data.blogList.concat(res.result)
-            })
+                blogList:this.data.blogList.concat(bloglist)
+                
+            })  
+        
             wx.hideLoading()
+        })
+    },
+    // 在小程序端调用云数据库
+    // 方法2
+    // getBlogListByminPro:function(){
+    //     blogCollection
+    //     .skip(this.data.blogList.length)
+    //     .limit(MAX_LILMIT)
+    //     .orderBy('createTime', 'desc')
+    //     .get()
+    //     .then((res)=>{
+    //         let bloglist=res.data
+    //         for(let i=0,len=bloglist.length;i<len;i++){
+    //             // bloglist[i].createTime=bloglist[i].toString()
+    //             blogList[i].createTime = formatTime(new Date(blogList[i].createTime))
+    //         }
+    //         this.setData({
+    //             blogList:this.data.blogList.concat(bloglist)
+                
+    //         })  
+    //         wx.hideLoading()
+    //     })
+    // },
+    // 跳转到详情页面
+    goComment:function(e){
+        wx.navigateTo({
+          url: `../blog-comment/blog-comment?blogId=${e.target.dataset.blogid}`,
         })
     }
 })
